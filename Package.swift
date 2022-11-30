24ba4a79da3201ada7dde5ef29b55cdaffd47d5f
Josef Zoller <josef@walterzollerpiano.com>
2022-11-30 14:06:14 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AoC",
+    platforms: [
+        .macOS(.v13)
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-numerics.git", from: "1.0.0"),
+        .package(url: "https://github.com/davecom/SwiftGraph.git", from: "3.1.0")
+    ],
+    targets: (1...25).map {
+        .executableTarget(
+            name: "Day\($0)",
+            dependencies: [
+                .product(name: "Algorithms", package: "swift-algorithms"),
+                .product(name: "Collections", package: "swift-collections"),
+                .product(name: "Numerics", package: "swift-numerics"),
+                "SwiftGraph"
+            ],
+            resources: [.copy("Resources/day\($0).txt")],
+            swiftSettings: [
+                .unsafeFlags(["-enable-bare-slash-regex"])
+            ]
+        )
+    }
+)
---
+++ Sources/Day22/main.swift
@@ -0,0 +1,11 @@
+import Algorithms
+import Collections
+import Foundation
+import Numerics
+import SwiftGraph
+
+let url = Bundle.module.url(forResource: "day22", withExtension: "txt")!
+let fileContents = try! String(contentsOf: url, encoding: .utf8)
+
+
+let lines = fileContents.split(separator: "\n")
