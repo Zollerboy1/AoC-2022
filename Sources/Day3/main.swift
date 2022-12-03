8366a531c2ced1d32352eab6cb911613b7acb3ff
Josef Zoller <josef@walterzollerpiano.com>
2022-12-03 08:17:20 +0100

    Add solution for day 3

--- Sources/Day3/main.swift
+++ Sources/Day3/main.swift
@@ -10,3 +10,28 @@ let fileContents = try! String(contentsOf: url, encoding: .utf8)
 
 
 let lines = fileContents.split(separator: "\n")
+
+// Part 1
+print(lines.map { line -> Int in
+    let count = line.count
+    let middle = line.index(line.startIndex, offsetBy: count / 2)
+
+    for item in line[line.startIndex..<middle] {
+        if line[middle..<line.endIndex].contains(item) {
+            return Array(" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").firstIndex(of: item)!
+        }
+    }
+
+    fatalError()
+}.sum())
+
+// Part 2
+print(lines.chunks(ofCount: 3).map(Array.init).map { group -> Int in
+    for character in group[0] {
+        if group[1].contains(character) && group[2].contains(character) {
+            return Array(" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").firstIndex(of: character)!
+        }
+    }
+
+    fatalError()
+}.sum())
