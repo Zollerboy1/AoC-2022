6fb00044c182e90ef13d4cc2180eb78d17910ab7
Josef Zoller <josef@walterzollerpiano.com>
2022-12-03 08:17:10 +0100

    Add solution for day 2

--- Sources/Day2/main.swift
+++ Sources/Day2/main.swift
@@ -10,3 +10,29 @@ let fileContents = try! String(contentsOf: url, encoding: .utf8)
 
 
 let lines = fileContents.split(separator: "\n")
+
+
+func score(_ opponent: Substring, _ me: Substring) -> Int {
+    switch me {
+        case "X": return 0
+        case "Y": return 3
+        default: return 6
+    }
+}
+
+func value(_ opponent: Substring, _ me: Substring) -> Int {
+    switch (opponent, me) {
+        case ("A", "Y"), ("B", "X"), ("C", "Z"): return 1
+        case ("A", "Z"), ("B", "Y"), ("C", "X"): return 2
+        default: return 3
+    }
+}
+
+
+print(lines.map { line in
+    let split = line.split(separator: " ")
+    let opponent = split[0]
+    let me = split[1]
+
+    return score(opponent, me) + value(opponent, me)
+}.reduce(0, +))
