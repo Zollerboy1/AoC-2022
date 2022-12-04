456bd74326ef0b6412b5ab3ae03e2b6829a5c13d
Josef Zoller <josef@walterzollerpiano.com>
2022-12-04 17:58:58 +0100

    Add solution for day 4

--- Sources/Day4/main.swift
+++ Sources/Day4/main.swift
@@ -10,3 +10,19 @@ let fileContents = try! String(contentsOf: url, encoding: .utf8)
 
 
 let lines = fileContents.split(separator: "\n")
+
+let regex = /^(\d+)-(\d+),(\d+)-(\d+)$/
+
+let ranges = lines.compactMap { line -> (ClosedRange<Int>, ClosedRange<Int>)? in
+    guard let match = line.matches(of: regex).first else { return nil }
+
+    return (Int(match.1)!...Int(match.2)!, Int(match.3)!...Int(match.4)!)
+}
+
+print(ranges.reduce(0) { acc, ranges in
+    return acc + (ranges.0.contains(ranges.1) || ranges.1.contains(ranges.0) ? 1 : 0)
+})
+
+print(ranges.reduce(0) { acc, ranges in
+    return acc + (ranges.0.overlaps(ranges.1) ? 1 : 0)
+})
