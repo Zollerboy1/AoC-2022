f90620d53f399037e888d72c037dae08b7966adb
Josef Zoller <josef@walterzollerpiano.com>
2022-12-06 23:53:00 +0100

    Add solution for day 6

--- Sources/Day6/main.swift
+++ Sources/Day6/main.swift
@@ -9,4 +9,10 @@ let url = Bundle.module.url(forResource: "day6", withExtension: "txt")!
 let fileContents = try! String(contentsOf: url, encoding: .utf8)
 
 
-let lines = fileContents.split(separator: "\n")
+let result = { n in
+    let windowed = fileContents.windows(ofCount: n)
+    return windowed.firstIndex { Set($0).count == n }.map { windowed.distance(from: windowed.startIndex, to: $0) }.map { $0 + n }
+}
+
+print(result(4)!)
+print(result(14)!)
