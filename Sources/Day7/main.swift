a887ec3cd4e06789e48365a20bf9bef757cfff5d
Josef Zoller <josef@walterzollerpiano.com>
2022-12-07 17:29:48 +0100

    Add solution for day 7

--- Sources/Day7/main.swift
+++ Sources/Day7/main.swift
@@ -5,8 +5,68 @@ import Foundation
 import Numerics
 import SwiftGraph
 
+
+class Directory {
+    let name: Substring
+    let parent: Directory?
+    var size: Int
+    var children: [Directory]
+
+    private var _totalSize: Int?
+
+    var totalSize: Int {
+        if let totalSize = self._totalSize {
+            return totalSize
+        } else {
+            let size = self.size + self.children.map(\.totalSize).sum()
+            self._totalSize = size
+            return size
+        }
+    }
+
+    var allSizes: [Int] {
+        [self.totalSize] + self.children.flatMap(\.allSizes)
+    }
+
+    init(name: Substring, parent: Directory?) {
+        self.name = name
+        self.parent = parent
+        self.size = 0
+        self.children = []
+    }
+}
+
+
 let url = Bundle.module.url(forResource: "day7", withExtension: "txt")!
 let fileContents = try! String(contentsOf: url, encoding: .utf8)
 
 
 let lines = fileContents.split(separator: "\n")
+
+var topDirectory = Directory(name: "/", parent: nil)
+var currentDirectory = topDirectory
+
+for line in lines {
+    if let cdMatch = line.wholeMatch(of: /\$ cd ([a-zA-Z]+|\.\.|\/)/) {
+        switch (cdMatch.1) {
+        case "/":
+            currentDirectory = topDirectory
+        case "..":
+            currentDirectory = currentDirectory.parent!
+        default:
+            currentDirectory = currentDirectory.children.first(where: { $0.name == cdMatch.1 })!
+        }
+    } else if line != "$ ls" {
+        if let fileMatch = line.wholeMatch(of: /([0-9]+) [a-zA-Z\.]+/) {
+            currentDirectory.size += Int(fileMatch.1)!
+        } else if let dirMatch = line.wholeMatch(of: /dir ([a-zA-Z]+)/) {
+            currentDirectory.children.append(Directory(name: dirMatch.1, parent: currentDirectory))
+        }
+    }
+}
+
+// Part 1
+print(topDirectory.allSizes.filter { $0 <= 100_000 }.sum())
+
+// Part 2
+print(topDirectory.allSizes.filter { $0 >= topDirectory.totalSize - 40000000 }.min()!)
