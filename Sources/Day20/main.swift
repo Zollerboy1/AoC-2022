a8102b1c90a8b633532893c1f6876c05c21d9781
Josef Zoller <josef@walterzollerpiano.com>
2022-12-22 09:30:55 +0100

    Add solution for day 20

--- Sources/Day20/main.swift
+++ Sources/Day20/main.swift
@@ -9,3 +9,23 @@ let fileContents = try! String(contentsOf: URL(filePath: "Sources/Day20/Resource
 
 
 let lines = fileContents.split(separator: "\n")
+
+var numbers = lines.map { Int($0)! }
+
+func solution(numbers: [Int], iterations: Int) -> Int {
+    var numbers = Array(numbers.enumerated())
+    for _ in 0..<iterations {
+        for i in 0..<numbers.count {
+            let indexBefore = numbers.firstIndex { $0.offset == i }!
+            let indexAfter = (((indexBefore + numbers[indexBefore].element) % (numbers.count - 1)) + (numbers.count - 1)) % (numbers.count - 1)
+            numbers.insert(numbers.remove(at: indexBefore), at: indexAfter)
+        }
+    }
+
+    let zeroIndex = numbers.firstIndex { $0.element == 0 }!
+
+    return numbers[(zeroIndex + 1000) % numbers.count].element + numbers[(zeroIndex + 2000) % numbers.count].element + numbers[(zeroIndex + 3000) % numbers.count].element
+}
+
+print(solution(numbers: numbers, iterations: 1))
+print(solution(numbers: numbers.map { $0 * 811589153 }, iterations: 10))
